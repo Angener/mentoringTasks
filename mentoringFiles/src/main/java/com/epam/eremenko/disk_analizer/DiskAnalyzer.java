d6fbd9478d4e95daf555207668f1ded38e637ccb
Andrei Eremenko <angennere@gmail.com>
2020-05-18 09:33:16 +0300

    in progress

--- mentoringFiles/src/main/java/com/epam/eremenko/disk_analizer/DiskAnalyzer.java
+++ mentoringFiles/src/main/java/com/epam/eremenko/disk_analizer/DiskAnalyzer.java
@@ -8,34 +8,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.function.Consumer;
 
 public class DiskAnalyzer {
     private static String result;
     private static int sCounter = 0;
     private static final List<File> list = new ArrayList<>();
-    private static final Consumer<List<File>> theBiggestLettersQuantity = (list) ->
-            list.stream()
-                    .filter(file -> file.getName().toLowerCase().contains("s"))
-                    .forEach(
-                            (file) -> {
-                                int occurrences = StringUtils.countMatches(file.getName(), "s");
-                                if (occurrences > sCounter) {
-                                    sCounter = occurrences;
-                                    result = file.getAbsolutePath();
-                                }
-                            });
-
-    private static final Consumer<List<File>> theFiveBiggestFiles = (list) -> {
-        list.stream()
-
-                .sorted(Comparator.comparingLong(File::length))
-                .peek(System.out::println)
-                .takeWhile(n -> n.equals(list.get(6)))
-                .peek(file -> System.out.println(file.getName()))
-                .forEach(file -> result += file.getName() + "\n");
-    };
-
 
 
     public static void get(String path, String number) {
@@ -43,8 +20,8 @@ public class DiskAnalyzer {
         readFiles(directory);
 
         switch (number) {
-            case ("1") -> theBiggestLettersQuantity.accept(list);
-            case ("2") -> theFiveBiggestFiles.accept(list);
+            case ("1") -> System.out.println(getTheBiggestLetterQuantityFile());
+            case ("2") -> System.out.println(getTheFiveBiggestFiles());
             default -> throw new IllegalArgumentException("Unknown choice");
         }
     }
@@ -63,11 +40,36 @@ public class DiskAnalyzer {
         }
     }
 
+    private static String getTheBiggestLetterQuantityFile() {
+        findFileWithMaxSLetters();
+        return result;
+    }
+
+    private static void findFileWithMaxSLetters() {
+        list.stream()
+                .filter(file -> file.getName().toLowerCase().contains("s"))
+                .forEach(
+                        (file) -> {
+                            int occurrences = StringUtils.countMatches(file.getName(), "s");
+                            if (occurrences > sCounter) {
+                                sCounter = occurrences;
+                                result = file.getAbsolutePath();
+                            }
+                        });
+    }
+
+    private static String getTheFiveBiggestFiles() {
+        StringBuilder sb = new StringBuilder();
+        list.sort(Comparator.comparing(File::length));
+        for (int i = list.size() - 1; i > list.size() - 6; i--) {
+            sb.append(list.get(i).getAbsolutePath()).append("\n");
+        }
+        return sb.toString();
+    }
+
+
 
     public static void main(String[] args) {
-
-
         get("/Users/angener/Documents", "2");
-        System.out.println(result);
     }
 }
